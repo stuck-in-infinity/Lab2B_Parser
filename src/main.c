feb0d7029ee8ade2cade5d3cb0dbccea82eb7595
Myth <mcs252102@iitd.ac.in>
2025-12-23 11:21:39 +0530

    simplified
    Refactor main function to simplify input handling and update output messages.

--- src/main.c
+++ src/main.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "ast.h"
 
-extern int yyparse(void);
-extern FILE *yyin;
-extern AST *root;
+int yyparse(void);
 
-int main(int argc, char **argv) {
-    if (argc > 1) {
-        yyin = fopen(argv[1], "r");
-        if (!yyin) {
-            perror("fopen");
-            return 1;
-        }
-    } else {
-        yyin = stdin;
-    }
+int main()
+{
+  printf("Starting parser...\n");
 
-    if (yyparse() == 0) {
-        printf("=== Parse successful ===\n");
-        if (root) {
-            print_ast(root, 0);
-            free_ast(root);
-        }
-    } else {
-        fprintf(stderr, "=== Parse failed ===\n");
-    }
+  if (yyparse() == 0)
+    printf("Parsing successful\n");
+  else
+    printf("Parsing failed\n");
 
-    if (yyin != stdin) fclose(yyin);
-    return 0;
+  return 0;
 }
+
