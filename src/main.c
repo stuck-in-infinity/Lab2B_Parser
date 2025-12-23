c77999860328a3092a8f78851d5530317badf4f1
Myth <mcs252102@iitd.ac.in>
2025-12-23 14:49:17 +0530

    ast being called in main now

--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include "ast.h"
 
-int yyparse(void);
+extern ASTNode *root;
+int yyparse();
 
 int main()
 {
   printf("Starting parser...\n");
-
   if (yyparse() == 0)
+  {
     printf("Parsing successful\n");
+    print_ast(root, 0);
+  }
   else
+  {
     printf("Parsing failed\n");
-
+  }
   return 0;
 }
-
