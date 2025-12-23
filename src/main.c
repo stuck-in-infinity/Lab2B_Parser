6868e6e817dbbf93a7455c498c5516fd6affc851
akshat163 <91406652+akshat163@users.noreply.github.com>
2025-12-24 00:18:19 +0530

    Added lines to handle empty file

--- src/main.c
+++ src/main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ast.h"
 
 extern ASTNode *root;
 int yyparse();
 
-int main()
-{
-  printf("Starting parser...\n");
-  if (yyparse() == 0)
-  {
-    printf("Parsing successful\n");
-    print_ast(root, 0);
-  }
-  else
-  {
-    printf("Parsing failed\n");
-  }
-  return 0;
+int main() {
+    printf("Starting parser...\n");
+    
+    int result = yyparse();
+    
+    if (result == 0) {
+        printf("Parsing successful!\n\n");
+        printf("Abstract Syntax Tree:\n");
+        printf("\n");
+        if (root) {
+            print_ast(root, 0);
+        } else {
+            printf("(empty program)\n");
+        }
+    } else {
+        printf("Parsing failed!\n");
+        return 1;
+    }
+    
+    return 0;
 }
