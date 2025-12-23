79f7a90ac30906b8cfbf62dcf0eda9c7375522c2
Myth <mcs252102@iitd.ac.in>
2025-12-23 11:17:25 +0530

    Refactor

--- src/ast.c
+++ src/ast.c
@@ -3,82 +3,89 @@
 #include <string.h>
 #include "ast.h"
 
-static ASTNode *new_node(NodeType type)
+static void indent(int n)
+{
+  for (int i = 0; i < n; i++)
+    printf("  ");
+}
+
+ASTNode *make_var_decl(char *name, ASTNode *value)
 {
   ASTNode *n = malloc(sizeof(ASTNode));
-  n->type = type;
-  n->name = NULL;
-  n->value = 0;
-  n->op = 0;
-  n->left = n->right = NULL;
-  return n;
-}
-
-ASTNode *create_var_decl(char *name, ASTNode *value)
-{
-  ASTNode *n = new_node(NODE_VAR_DECL);
+  n->type = NODE_VAR_DECL;
   n->name = strdup(name);
   n->left = value;
+  n->right = NULL;
   return n;
 }
 
-ASTNode *create_assign(char *name, ASTNode *value)
+ASTNode *make_assign(char *name, ASTNode *value)
 {
-  ASTNode *n = new_node(NODE_ASSIGN);
+  ASTNode *n = malloc(sizeof(ASTNode));
+  n->type = NODE_ASSIGN;
   n->name = strdup(name);
   n->left = value;
+  n->right = NULL;
   return n;
 }
 
-ASTNode *create_binop(char op, ASTNode *left, ASTNode *right)
+ASTNode *make_binop(char op, ASTNode *left, ASTNode *right)
 {
-  ASTNode *n = new_node(NODE_BINOP);
+  ASTNode *n = malloc(sizeof(ASTNode));
+  n->type = NODE_BINOP;
   n->op = op;
   n->left = left;
   n->right = right;
   return n;
 }
 
-ASTNode *create_number(int value)
+ASTNode *make_number(int value)
 {
-  ASTNode *n = new_node(NODE_NUMBER);
+  ASTNode *n = malloc(sizeof(ASTNode));
+  n->type = NODE_NUMBER;
   n->value = value;
+  n->left = n->right = NULL;
   return n;
 }
 
-ASTNode *create_ident(char *name)
+ASTNode *make_ident(char *name)
 {
-  ASTNode *n = new_node(NODE_IDENT);
+  ASTNode *n = malloc(sizeof(ASTNode));
+  n->type = NODE_IDENT;
   n->name = strdup(name);
+  n->left = n->right = NULL;
   return n;
 }
 
-void print_ast(ASTNode *node, int indent)
+void print_ast(ASTNode *node, int indent_lvl)
 {
   if (!node)
     return;
 
-  for (int i = 0; i < indent; i++)
-    printf("  ");
+  indent(indent_lvl);
 
   switch (node->type)
   {
   case NODE_VAR_DECL:
     printf("VAR_DECL(%s)\n", node->name);
-    print_ast(node->left, indent + 1);
+    print_ast(node->left, indent_lvl + 1);
     break;
+
   case NODE_ASSIGN:
     printf("ASSIGN(%s)\n", node->name);
-    print_ast(node->left, indent + 1);
+    print_ast(node->left, indent_lvl + 1);
     break;
+
   case NODE_BINOP:
     printf("BINOP(%c)\n", node->op);
-    print_ast(node->left, indent + 1);
-    print_ast(node->right, indent + 1);
+    print_ast(node->left, indent_lvl + 1);
+    print_ast(node->right, indent_lvl + 1);
     break;
+
   case NODE_NUMBER:
     printf("NUMBER(%d)\n", node->value);
     break;
+
   case NODE_IDENT:
     printf("IDENT(%s)\n", node->name);
     break;
